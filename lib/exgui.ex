3ac2c62ff85957075dfbeaf1210bf2877a36ee7a
Karl Eklund <localpart@gmail.com>
2023-04-20 00:26:55 +0200

    Add Rustler

--- lib/exgui.ex
+++ lib/exgui.ex
@@ -15,4 +15,9 @@ defmodule Exgui do
   def hello do
     :world
   end
+
+  use Rustler, otp_app: :exgui, crate: "exgui"
+
+  # When your NIF is loaded, it will override this function.
+  def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
 end
--- test/exgui_test.exs
+++ test/exgui_test.exs
@@ -5,4 +5,8 @@ defmodule ExguiTest do
   test "greets the world" do
     assert Exgui.hello() == :world
   end
+
+  test "adding by Rustler NIF" do
+    assert Exgui.add(1, 2) == 3
+  end
 end
